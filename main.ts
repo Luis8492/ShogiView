7bd6dc2f60efe470209fcd58b4de8b54e669b5b7
Luis8492 <130156566+Luis8492@users.noreply.github.com>
2025-09-20 20:32:05 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,134 +1,89 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+import { Plugin, MarkdownPostProcessorContext } from 'obsidian';
+}
+if (lastTo && lastTo.f===f && lastTo.r===r) cell.addClass('highlight-to');
+if (lastFrom && lastFrom.f===f && lastFrom.r===r) cell.addClass('highlight-from');
+}
+}
+const info = [
+header['棋戦']?`棋戦: ${header['棋戦']}`:undefined,
+header['戦型']?`戦型: ${header['戦型']}`:undefined,
+moveIdx>0 && moves[moveIdx-1]?.timestamp?`消費時間: ${moves[moveIdx-1].timestamp}`:undefined
+].filter(Boolean).join(' / ');
+meta.setText(info);
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+
+function applyUpTo(idx:number){
+board = initialBoard();
+lastFrom = lastTo = undefined;
+// Very naive move applier: uses explicit (from) if present, ignores promotions/drops, handles simple capture
+for (let i=0; i<idx; i++) {
+const mv = moves[i];
+if (!mv) break;
+const from = mv.from;
+const to = mv.to;
+if (!from) continue; // MVP: require (xx) source
+const src = board[from.r-1][from.f-1];
+if (!src) continue;
+// move piece
+board[to.r-1][to.f-1] = src; // capture if any
+board[from.r-1][from.f-1] = null;
+lastFrom = from; lastTo = to;
+}
+moveIdx = idx;
+renderBoard();
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+btnFirst.onclick = ()=> applyUpTo(0);
+btnPrev.onclick = ()=> applyUpTo(Math.max(0, moveIdx-1));
+btnNext.onclick = ()=> applyUpTo(Math.min(moves.length, moveIdx+1));
+btnLast.onclick = ()=> applyUpTo(moves.length);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+// initial render
+applyUpTo(0);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+// If there are inline comments (*) after a move, show them beneath
+const commentsDiv = container.createDiv({ cls: 'meta' });
+const updateComments = () => {
+commentsDiv.empty();
+if (moveIdx>0) {
+const mv = moves[moveIdx-1];
+if (mv?.comment) {
+const pre = commentsDiv.createEl('pre');
+pre.textContent = mv.comment;
+}
+}
+};
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+// patch buttons to also refresh comments
+const wrap = (fn: ()=>void) => () => { fn(); updateComments(); };
+btnFirst.onclick = wrap(()=>applyUpTo(0));
+btnPrev.onclick = wrap(()=>applyUpTo(Math.max(0, moveIdx-1)));
+btnNext.onclick = wrap(()=>applyUpTo(Math.min(moves.length, moveIdx+1)));
+btnLast.onclick = wrap(()=>applyUpTo(moves.length));
+updateComments();
+}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+// =============================
+// Usage in a note
+// =============================
+// ```kif
+// # ---- ぴよ将棋 棋譜ファイル ----
+// 棋戦：ぴよ将棋
+// 戦型：△ツノ銀雁木
+// 開始日時：2025/09/20 17:38:04
+// 終了日時：2025/09/20 17:54:54
+// 手合割：平手
+// 先手：プレイヤー
+// 後手：プレイヤー
+// 手数----指手---------消費時間--
+// 1 ２六歩(27)( 0:12/00:00:12)
+// 2 ３四歩(33)( 0:05/00:00:05)
+// 3 ２五歩(26)( 0:01/00:00:13)
+// ```
